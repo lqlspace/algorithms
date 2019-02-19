4bca748f72b9c7f2fffba96faa8ed6003d8107a0
liuql <liuql_cs@163.com>
2019-02-19 11:16:40 +0800

    添加heap sort算法

---
+++ algorithms/sorting/heap_sort_test.go
@@ -0,0 +1,21 @@
+package sorting
+
+import (
+	"testing"
+	"algorithm-data-structure/algorithms/rand"
+)
+
+
+func TestHeapSort(t *testing.T) {
+	items := randnum.RandomIntSlice(30)
+	t.Log(items)
+	HeapSort(items, len(items))
+	for i := 0; i < len(items)-1; i++ {
+		if items[i] > items[i+1] {
+			t.Error("[error] heap sort error!")
+			return
+		}
+	}
+	t.Log(items)
+	t.Log("heap sort succeed!")
+}
