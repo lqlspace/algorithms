204aa40aee1fabccd3f54975fd22a8c3b096858b
liuql <liuql_cs@163.com>
2019-02-20 22:32:17 +0800

    添加Ring

--- data_structures/linked_list/doubly_llist.go
+++ data_structures/linked_list/doubly_llist.go
@@ -1,4 +1,15 @@
-//Doubly Linked List
+/*
+*双向链表：
+*（1）插入单个element时间复杂度O(1)
+*（2）删除单个element时间复杂度O(1)
+*（3）链表无法直接寻址，故访问特定位置需要从头遍历，时间复杂度为O(n)
+*（4）查找特定元素，从头遍历比较查询，时间复杂度O(n)
+*（5）排序，待定
+*（6）DLList结构中存有len，查询链表element总数的时间复杂度为O(1)
+*（7）通过list.root哨兵节点，查找头部节点的时间复杂度为O(1)
+*（8）通过list.root哨兵节点，查找尾部节点的时间复杂度为O(1)
+*（9）MoveToFront、MoveToBack的时间复杂度为O(1)
+*/
 package list
 
 type Element struct {
@@ -39,10 +50,12 @@ func New() *DLList {
 	return new(DLList).Init()
 }
 
+//通过List结构中存储len，返回链表长度的时间复杂度为O(1)
 func (l *DLList) Len() int {
 	return l.len
 }
 
+//通过哨兵节点l.root，返回头部element的时间复杂度为O(1)
 func (l *DLList) Front() *Element {
 	if l.len == 0 {
 		return nil
@@ -50,6 +63,8 @@ func (l *DLList) Front() *Element {
 	return l.root.next
 }
 
+
+//通过哨兵节点l.root，返回尾部element的时间复杂度为O(1)
 func (l *DLList) Back() *Element {
 	if l.len == 0 {
 		return nil
@@ -57,6 +72,7 @@ func (l *DLList) Back() *Element {
 	return l.root.prev
 }
 
+//插入一个element的时间复杂度为O(1)
 func (l *DLList) insert(elem, at *Element) *Element {
 	elem.prev = at
 	elem.next = at.next
@@ -68,25 +84,29 @@ func (l *DLList) insert(elem, at *Element) *Element {
 	return elem
 }
 
+//插入一个value的时间复杂度为O(1)
 func (l *DLList) insertValue(v interface{}, at *Element) *Element {
 	return l.insert(&Element{Value: v}, at)
 }
 
+//在头部插入一个element的时间复杂度为O(1)
 func (l *DLList) PushFront(val interface{}) *Element {
 	return l.insertValue(val, &l.root)
 }
 
+//在尾部插入一个element的时间复杂度为O(1)
 func (l *DLList) PushBack(val interface{}) *Element {
 	return l.insertValue(val, l.root.prev)
 }
 
-
+//在mark位置插入一个elment的时间复杂度为O(1)，但是定位到mark位置的时间复杂度为O(n)
 func (l *DLList) InsertBefore(v interface{}, mark *Element) *Element {
 	if mark.list != l {
 		return nil
 	}
 	return l.insertValue(v, mark.prev)
 }
+
 
 func (l *DLList) InsertAfter(v interface{}, mark *Element) *Element {
 	if mark.list != l {
@@ -95,6 +115,29 @@ func (l *DLList) InsertAfter(v interface{}, mark *Element) *Element {
 	return l.insertValue(v, mark)
 }
 
+
+//删除1个element的时间复杂度为O(1)
+func (l *DLList) remove(e *Element) *Element {
+	e.prev.next = e.next
+	e.next.prev = e.prev
+	e.next = nil
+	e.prev = nil
+	e.list = nil
+	l.len--
+
+	return e
+}
+
+
+//删除1个element的时间复杂度为O(1)
+func (l *DLList) Remove(e *Element) interface{} {
+	if e.list == l {
+		l.remove(e)
+	}
+	return e.Value
+}
+
+//移动一个element的时间复杂度为O(1)
 func (l *DLList) move(e, at *Element) *Element {
 	if e == at {
 		return e
@@ -111,6 +154,7 @@ func (l *DLList) move(e, at *Element) *Element {
 }
 
 
+//移动到头部的时间复杂度为O(1)
 func (l *DLList) MoveToFront(e *Element) {
 	if e.list != l || l.root.next == e {
 		return
@@ -119,6 +163,8 @@ func (l *DLList) MoveToFront(e *Element) {
 	l.move(e, &l.root)
 }
 
+
+//移动到尾部的时间复杂度为O(1)
 func (l *DLList) MoveToBack(e *Element) {
 	if e.list != l || l.root.prev == e {
 		return
@@ -126,6 +172,7 @@ func (l *DLList) MoveToBack(e *Element) {
 	l.move(e, l.root.prev)
 }
 
+//移动到mark位置之前的时间复杂度为O(1)
 func (l *DLList) MoveBefor(e, mark *Element) {
 	if e.list != l || e == mark || mark.list != l {
 		return
@@ -133,12 +180,14 @@ func (l *DLList) MoveBefor(e, mark *Element) {
 	l.move(e, mark.prev)
 }
 
+//移动到mark位置之后的时间复杂度为O(1)
 func (l *DLList) MoveAfter(e, mark *Element) {
 	if e.list != l || e == mark || mark.list != l {
 		return
 	}
 	l.move(e, mark)
 }
+
 
 func (l *DLList) PushBackList(other *DLList) {
 	for e := other.Front(); e != nil; e = e.Next() {
--- data_structures/linked_list/singly_llist.go
+++ data_structures/linked_list/singly_llist.go
@@ -1,3 +1,15 @@
+/*
+*单向链表：
+*（1）插入单个element的时间复杂度为O(1)
+*（2）删除单个element的时间复杂度为O(1)
+*（3）定位的指定位置的时间复杂度为O(n)
+*（4）查找某elment的时间复杂度为
+*（5）链表排序的时间复杂度为
+*（6）List中存有len，所以返回链表element总数的时间复杂度为O(1)
+*（7）定位到头部element的时间复杂度为O(1)
+*（8）定位到尾部节点的时间复杂度为O(n)
+*（9）移动某个element的时间复杂度为O(1)
+*/
 package list 
 
 
@@ -58,6 +70,30 @@ func (l *List) PushBack(val interface{}) *Element {
 	return e
 }
 
+
+func (l *List) Remove(val interface{}) {
+	if l.len == 0 {
+		return
+	} else if l.len == 0 {
+		if l.front.Value.(int) == val {
+			l.front = nil
+			l.len--
+		}
+	}
+	prev := l.front
+	next := prev.next
+	for next != nil {
+		if next.Value.(int) == val {
+			prev.next = next.next
+			l.len--
+			break
+		}
+		prev = next
+		next = next.next
+	}
+}
+
+
 //链表反转方法一：从第2个节点开始到第N个节点，依次进行删除后头部插入；
 //用到了两个指针first始终指向ReverseList操作前的第1个节点，e始终执行first的下一个节点；
 func (l *List) ReverseList() {
@@ -77,5 +113,25 @@ func (l *List) ReverseList() {
 	first.next = nil
 }
 
-//链表反转方法二：采用递归调用
+func (l *List) reverseListRecursive(e *Element) {
+	if e == nil {
+		return 
+	}
 
+	next := e.next
+	e.next = l.front
+	l.front = e
+	l.reverseListRecursive(next)
+
+}
+
+//链表反转方法二：采用递归调用
+func (l *List) ReverseListRecursive() {
+	if l.len <= 1 {
+		return
+	}
+
+	front := l.front
+	l.reverseListRecursive(front.next)
+	front.next = nil
+}
