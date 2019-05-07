7d84f26924de4f8de530a464cb22f01c43bc0517
liuql <liuql_cs@163.com>
2019-05-07 10:00:59 +0800

    添加希尔排序算法

--- algorithms/sorting/sort.go
+++ algorithms/sorting/sort.go
@@ -6,6 +6,7 @@ type Interface interface{
 	Swap(i, j int)
 }
 
+
 type IntSlice []int
 
 func (is IntSlice) Len() int {
@@ -20,14 +21,49 @@ func (is IntSlice) Swap(i, j int) {
 	is[j], is[i] = is[i], is[j]
 }
 
-func (is IntSlice) Sort() {
-	insertionSort(is, 0, len(is))
-}
 
-func insertionSort(data Interface, a, b int) {
-	for i := a + 1; i < b; i++ {
-		for j := i; j > a && data.Less(j, j-1); j-- {
-			data.Swap(j, j-1)
+
+//插入排序对于部分有效的数组十分高效，也很适合小规模数组
+func InsertionSort(arr Interface, a, b int) {
+	for i := a; i < b; i++ {
+		for j := i; j > a && arr.Less(j, j-1); j-- {
+			arr.Swap(j, j-1)
 		}
 	}
 }
+
+//希尔排序是对插入排序的改进，分组可缩小无序数组规模，而后合并成部分有序数组也可以发挥插入排序的长处
+func ShellSort(arr Interface, a, b int) {
+	n := b - a
+	if n < 2 {
+		return
+	}
+
+	key := n / 2
+	for key > 0 {
+		for i := key; i < n; i++ {
+			for j := i; j >= key && arr.Less(j,j-key); j -= key {
+				arr.Swap(j, j-key)
+			}
+		}
+		key = key / 2
+	}
+}
+
+//内层循环：遍历无序集合，找到min值对应索引下标;
+//外层循环：指定内层循环求出的min所在的索引，并交换；
+//时间复杂度：O(n^2)
+func SelectionSort(arr Interface, a, b int) {
+	for i := a; i < b; i++ {
+		min := i
+		for j := i + 1; j < b; j++ {
+			if arr.Less(j, min) {
+				min = j
+			}
+		}
+		arr.Swap(min, i)
+	}
+}
+
+
+
--- algorithms/sorting/sort_test.go
+++ algorithms/sorting/sort_test.go
@@ -5,7 +5,21 @@ import (
 )
 
 func TestInsertionSortIntSlice(t *testing.T) {
-	arr := IntSlice{3, 3, 9, 6, 2, 7, 7, 8, 1, 0}
-	arr.Sort()
-	t.Log(arr)
+	datas := IntSlice{3, -1, 3, 2, 5, 2, 7, 9}
+	InsertionSort(datas, 0, len(datas))
+	t.Log(datas)
+}
+
+
+func TestSelectionSortIntSlice(t *testing.T) {
+	datas := IntSlice{-1, -3, 9, 9, 7, 6, 3, 5, 7, 8}
+	SelectionSort(datas, 0, len(datas))
+	t.Log(datas)
+}
+
+
+func TestShellSortIntSlice(t *testing.T) {
+	datas := IntSlice{-1, 3, 2, 1, 2, 7, 6, 7, 9}
+	ShellSort(datas, 0, len(datas))
+	t.Log(datas)
 }
