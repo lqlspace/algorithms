a90e3e3d7497e257ab00761adf4531613e619e4f
liuql <liuql_cs@163.com>
2021-01-17 10:48:23 +0800

    feat[leetcode] 反转链表

---
+++ leetcode/linkedlist/206-反转链表.go
@@ -0,0 +1,39 @@
+package linkedlist
+
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+
+// 迭代法（时间复杂度O(n), 空间复杂度O(1)）
+func reverseListMethod1(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	sentinel := new(ListNode)
+	for head != nil {
+		tmp := head
+		head = head.Next
+		tmp.Next = sentinel.Next
+		sentinel.Next = tmp
+	}
+
+	return sentinel.Next
+}
+
+// 递归法（时间复杂度O(N), 空间复杂度O(N)）
+func reverseListMethod2(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+	pre := reverseListMethod2(head.Next)
+	head.Next.Next = head
+	head.Next = nil
+
+	return pre
+}
