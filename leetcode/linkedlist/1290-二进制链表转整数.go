a3c1114497b972a1f5b74bf3756a54cf65a1f08e
liuql <liuql_cs@163.com>
2021-01-17 11:56:03 +0800

    feat[leetcode] 二进制链表转整数

---
+++ leetcode/linkedlist/1290-二进制链表转整数.go
@@ -0,0 +1,18 @@
+package linkedlist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func getDecimalValue(head *ListNode) int {
+	var ans int
+	for head != nil {
+		ans = ans * 2 + head.Val
+		head = head.Next
+	}
+
+	return ans
+}
