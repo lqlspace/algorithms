3f041d9d100e11614e40fd1f50087c55a9529098
liuql <liuql_cs@163.com>
2021-01-18 10:20:28 +0800

    feat[leetcode] 相交链表

---
+++ leetcode/linkedlist/160-相交链表.go
@@ -0,0 +1,94 @@
+package linkedlist
+
+import (
+	"unsafe"
+)
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+// 借助map（时间复杂度O(N), 空间复杂度O(N)）
+func getIntersectionNode1(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	pos := make(map[unsafe.Pointer]struct{})
+	for headA != nil && headB != nil {
+		if _, ok := pos[unsafe.Pointer(headA)]; ok {
+			return headA
+		} else {
+			pos[unsafe.Pointer(headA)] = struct{}{}
+		}
+		if _, ok := pos[unsafe.Pointer(headB)]; ok {
+			return headB
+		} else {
+			pos[unsafe.Pointer(headB)] = struct{}{}
+		}
+		headA = headA.Next
+		headB = headB.Next
+	}
+	if headA == nil {
+		for headB != nil {
+			if _, ok := pos[unsafe.Pointer(headB)]; ok {
+				return headB
+			}
+			headB = headB.Next
+		}
+	}
+	if headB == nil {
+		for headA != nil {
+			if _, ok := pos[unsafe.Pointer(headA)]; ok {
+				return headA
+			}
+			headA = headA.Next
+		}
+	}
+
+	return nil
+}
+
+// 双循环(时间复杂度O(mn)，空间复杂度O(1))
+func getIntersectionNode2(headA, headB *ListNode) *ListNode {
+	if headA == nil || headB == nil {
+		return nil
+	}
+
+	for headA != nil {
+		p := headB
+		for p != nil {
+			if p == headA {
+				return p
+			}
+			p = p.Next
+		}
+		headA = headA.Next
+	}
+
+	return nil
+}
+
+// 双指针
+func getIntersectionNode3(headA, headB *ListNode) *ListNode {
+	pa, pb := headA, headB
+
+	for pa != pb {
+		if pa != nil {
+			pa = pa.Next
+		} else {
+			pa = headB
+		}
+		if pb != nil {
+			pb = pb.Next
+		} else {
+			pb = headA
+		}
+	}
+	return pa
+}
+
+
