5e683eb33e96f2701d86754252dc9ffba653aa0a
liuql <liuql_cs@163.com>
2021-01-20 08:55:28 +0800

    删除中间的某个节点

---
+++ leetcode/linkedlist/面试题02.03-删除中间的某个节点.go
@@ -0,0 +1,15 @@
+package linkedlist
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func deleteNode1(node *ListNode) {
+	if node != nil && node.Next != nil {
+		node.Val = node.Next.Val
+		node.Next = node.Next.Next
+	}
+}
