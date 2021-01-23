0a6bad2d69e601faa2f058eece9b6effacce3a60
liuql <liuql_cs@163.com>
2021-01-23 11:31:06 +0800

    删除链表的倒数第N个节点

--- leetcode/linkedlist/medium/19-删除链表的倒数第N个节点.go
+++ leetcode/linkedlist/medium/19-删除链表的倒数第N个节点.go
@@ -7,7 +7,7 @@ package medium
  *     Next *ListNode
  * }
  */
-// 两次遍历
+// 两次遍历(时间复杂度O(N)，空间复杂度O(1))
 func removeNthFromEnd1(head *ListNode, n int) *ListNode {
 	var num int
 	cur := head
@@ -30,6 +30,45 @@ func removeNthFromEnd1(head *ListNode, n int) *ListNode {
 	}
 
 	cur.Next = cur.Next.Next
+
+	return sentinel.Next
+}
+
+// 递归，时间复杂度O(N)，空间复杂度O(N)
+func removeNthFromEnd2(head *ListNode, n int) *ListNode {
+	var num int
+	var checkRecur func(cur *ListNode) *ListNode
+	checkRecur = func(cur *ListNode) *ListNode {
+		if cur != nil {
+			cur.Next = checkRecur(cur.Next)
+			num++
+			if num == n {
+				return cur.Next
+			}
+		}
+		return cur
+	}
+
+	return checkRecur(head)
+}
+
+// 双指针
+func removeNthFromEnd3(head *ListNode, n int) *ListNode {
+	sentinel := new(ListNode)
+	sentinel.Next = head
+	slow, fast := sentinel, sentinel
+	for i := 0; i < n+1; i++ {
+		if fast == nil {
+			return nil
+		}
+		fast = fast.Next
+	}
+
+	for fast != nil {
+		fast = fast.Next
+		slow = slow.Next
+	}
+	slow.Next = slow.Next.Next
 
 	return sentinel.Next
 }
