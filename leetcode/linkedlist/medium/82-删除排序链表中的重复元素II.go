aa4b12db3d75fe4e7586d81474b2a84384709559
liuql <liuql_cs@163.com>
2021-01-24 11:52:37 +0800

    删除排序链表中的重复元素II，hash表

--- leetcode/linkedlist/medium/82-删除排序链表中的重复元素II.go
+++ leetcode/linkedlist/medium/82-删除排序链表中的重复元素II.go
@@ -75,3 +75,29 @@ func deleteDuplicates2(head *ListNode) *ListNode {
 
 	return head
 }
+
+// 借助hash表
+func deleteDuplicates3(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+
+	hashMap := make(map[int]int)
+	cur := head
+	for cur != nil {
+		hashMap[cur.Val]++
+		cur = cur.Next
+	}
+
+	sentinel := &ListNode{Next:head}
+	cur = sentinel
+	for cur.Next != nil {
+		if val, ok := hashMap[cur.Next.Val]; ok && val > 1{
+				cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+
+	return sentinel.Next
+}
