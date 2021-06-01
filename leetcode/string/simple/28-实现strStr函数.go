b39c2cc3df00baaa08da599a3b2426bd5a4fb046
liuql <liuql_cs@163.com>
2021-06-01 10:08:49 +0800

    feat: 添加KMP算法

--- leetcode/string/simple/28-实现strStr函数.go
+++ leetcode/string/simple/28-实现strStr函数.go
@@ -1,21 +1,71 @@
 package simple
 
-// 1、滑动窗口暴力法，时间复杂度O(（N-L）L)
+// 1、滑动窗口暴力法，时间复杂度O(n*m)
 func strStr1(haystack string, needle string) int {
-	l1 := len(haystack)
-	l2 := len(needle)
-	if l1 < l2 {
-		return -1
+	n, m := len(haystack), len(needle)
+	if m == 0 {
+		return 0
 	}
 
-	var pos = -1
-	for i := 0; i < l1-l2+1; i++ {
-		str := haystack[i:i+l2]
-		if str == needle {
-			pos = i
-			break
+outer:
+	for i := 0; i <= n - m; i++ {
+		for j := 0; j < m; j++ {
+			if haystack[i+j] != needle[j] {
+				continue outer
+			}
+		}
+		return i
+	}
+
+	return -1
+}
+
+// 2、KMP算法，时间复杂度O（n+m）
+func strStr2(haystack string, needle string) int {
+	n, m := len(haystack), len(needle)
+	if m == 0 {
+		return 0
+	}
+
+	next := make([]int, m)
+	for i, j := 1, 0; i < m; i++ {
+		for j > 0 && needle[i] != needle[j] {
+			j = next[j-1]
+		}
+		if needle[i] == needle[j] {
+			j++
+		}
+		next[i] = j
+	}
+
+	for i, j := 0, 0; i < n; i++ {
+		for j > 0 && haystack[i] != needle[j] {
+			j = next[j-1]
+		}
+		if haystack[i] == needle[j] {
+			j++
+		}
+		if j == m {
+			return i - m + 1
 		}
 	}
 
-	return pos
+	return -1
+}
+
+func strStr3(haystack, needle string) int {
+	n, m := len(haystack), len(needle)
+	for i := 0; i <= n - m; i++ {
+		flag := true
+		for j := range needle {
+			if haystack[i+j] != needle[j] {
+				flag = false
+				break
+			}
+		}
+		if flag {
+			return i
+		}
+	}
+	return -1
 }
--- leetcode/string/simple/最后一个单词的长度_test.go
+++ leetcode/string/simple/最后一个单词的长度_test.go
@@ -8,3 +8,31 @@ func Test_LengthOfLastWord(t *testing.T) {
 	l := lengthOfLastWord("a")
 	t.Log(l)
 }
+
+
+func Test_strStr2(t *testing.T)  {
+	hayStack := "abcbd"
+	needle := "cb"
+
+	idx := strStr2(hayStack, needle)
+	t.Log(idx)
+
+	needle = "bc"
+	idx = strStr2(hayStack, needle)
+	t.Log(idx)
+}
+
+func Test_strStr3(t *testing.T)  {
+	haystack := "sdfsdef"
+	needle := "fs"
+
+	pos := strStr3(haystack, needle)
+	t.Log(pos)
+
+	needle = "sde"
+	pos = strStr3(haystack, needle)
+	t.Log(pos)
+
+	pos = strStr1(haystack, needle)
+	t.Log(pos)
+}
