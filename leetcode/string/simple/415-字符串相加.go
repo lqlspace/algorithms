b6dbd1504fc9d6c03b50ad91a44a31ce90b9115e
liuql <liuql_cs@163.com>
2021-06-04 09:29:37 +0800

    字符串相加

--- leetcode/string/simple/415-字符串相加.go
+++ leetcode/string/simple/415-字符串相加.go
@@ -1,20 +1,19 @@
 package simple
 
-func addString(num1 string, num2 string) string {
+func addString(num1, num2 string) string {
 	lenNum1, lenNum2 := len(num1), len(num2)
 	n := max(lenNum1, lenNum2)
 
-	var carry int
+	var carry uint8
 	res := make([]byte, n)
 	for i := 0; i < n; i++ {
 		if i < lenNum1 {
-			carry += int(num1[lenNum1-1-i] - '0')
+			carry += num1[lenNum1-1-i] - '0'
 		}
 		if i < lenNum2 {
-			carry += int(num2[lenNum2-1-i] - '0')
+			carry += num2[lenNum2-1-i] - '0'
 		}
-
-		res[n-1-i] = byte(carry % 10 + '0')
+		res[n-1-i] = carry % 10 + '0'
 		carry /= 10
 	}
 	if carry > 0 {
