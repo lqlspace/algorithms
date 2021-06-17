eca0c642900d20b50122dd733b883dfc6ec73183
liuql <liuql_cs@163.com>
2021-06-17 10:10:35 +0800

    反转字符串中的单词III

--- leetcode/string/simple/557-反转字符串中的单词.go
+++ leetcode/string/simple/557-反转字符串中的单词.go
@@ -1,22 +1,26 @@
 package simple
 
-// 时间复杂度O(N), 空间复杂度O(1)
+// 时间复杂度O(N), 空间复杂度O(N)
 func reverseWords(s string) string {
-	length := len(s)
+	n := len(s)
 	arr := []byte(s)
-	for i := 0; i < length; {
+
+	for i := 0; i < n; {
 		start := i
-		for i < length && s[i] != ' ' {
+		for i < n && s[i] != ' ' {
 			i++
 		}
-		for j, k := start, i-1; j < k; j++ {
-			arr[j], arr[k]  = arr[k], arr[j]
+
+		for j, k := start, i-1; j < k; {
+			arr[j], arr[k] = arr[k], arr[j]
+			j++
 			k--
 		}
 
-		for i < length && s[i] == ' ' {
+		for i < n && s[i] == ' ' {
 			i++
 		}
 	}
+
 	return string(arr)
 }
