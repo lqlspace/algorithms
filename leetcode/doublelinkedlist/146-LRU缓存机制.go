137a61d7a98ecb347b6a41f49f4cbf041db3a4ac
liuql <liuql_cs@163.com>
2021-06-26 10:47:32 +0800

    LRU缓存机制

--- leetcode/doublelinkedlist/146-LRU缓存机制.go
+++ leetcode/doublelinkedlist/146-LRU缓存机制.go
@@ -7,18 +7,20 @@ type LRUCache struct {
 	head, tail *DLinkedNode
 }
 
+
 type DLinkedNode struct {
-	key, value int
+	key, val int
 	pre, next *DLinkedNode
 }
+
 
 
 func Constructor(capacity int) LRUCache {
 	l := LRUCache{
 		capacity: capacity,
-		cache:    make(map[int]*DLinkedNode),
-		head:     new(DLinkedNode),
-		tail:     new(DLinkedNode),
+		cache:    map[int]*DLinkedNode{},
+		head:     &DLinkedNode{},
+		tail:     &DLinkedNode{},
 	}
 	l.head.next = l.tail
 	l.tail.pre = l.head
@@ -26,58 +28,58 @@ func Constructor(capacity int) LRUCache {
 	return l
 }
 
+func (this *LRUCache) addToHead(node *DLinkedNode) {
+	node.pre = this.head
+	node.next = this.head.next
+	node.pre.next = node
+	node.next.pre = node
+}
+
+func (this *LRUCache) removeNode(node *DLinkedNode) {
+	node.pre.next = node.next
+	node.next.pre = node.pre
+}
+
+func (this *LRUCache) moveToHead(node *DLinkedNode) {
+	this.removeNode(node)
+	this.addToHead(node)
+}
+
+func (this *LRUCache) removeFromTail() *DLinkedNode {
+	node := this.tail.pre
+	node.pre.next = node.next
+	node.next.pre = node.pre
+
+	return node
+}
 
 func (this *LRUCache) Get(key int) int {
-	// 1. 从map查询
 	node, ok := this.cache[key]
 	if !ok {
 		return -1
 	}
 
-	// 2. 找到，则把该节点写入到头部(删除，插入)
-	node.pre.next = node.next
-	node.next.pre = node.pre
+	this.moveToHead(node)
 
-	node.pre = this.head
-	node.next = this.head.next
-	node.next.pre = node
-	node.pre.next = node
-
-	return node.value
+	return node.val
 }
 
 
 func (this *LRUCache) Put(key int, value int)  {
-	// 1. 写入map
-	if node, ok := this.cache[key]; ok {
-		node.value = value
-
-		// 2. 添加到链表头部
-		node.pre.next = node.next
-		node.next.pre = node.pre
-		node.pre = this.head
-		node.next = this.head.next
-		node.next.pre = node
-		node.pre.next = node
+	node, ok := this.cache[key]
+	if ok {
+		node.val = value
+		this.moveToHead(node)
 		return
 	}
-	node := &DLinkedNode{key: key, value: value}
+
+	node = &DLinkedNode{key: key, val: value}
+	this.addToHead(node)
 	this.cache[key] = node
-
-	// 2. 添加到链表头部
-	node.pre = this.head
-	node.next = this.head.next
-	node.next.pre = node
-	node.pre.next = node
-
-	// 3. size加1
 	this.size++
 
-	// 4. 判断size有没有超过capacity,超过则删除末尾节点
 	if this.size > this.capacity {
-		n := this.tail.pre
-		n.pre.next = n.next
-		n.next.pre = n.pre
+		n := this.removeFromTail()
 		delete(this.cache, n.key)
 		this.size--
 	}
