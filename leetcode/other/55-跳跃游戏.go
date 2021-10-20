e64e8d323584ce133e83680bc9b56eb91551e7bd
liuql <liuql_cs@163.com>
2021-10-20 19:08:37 +0800

    跳跃游戏

---
+++ leetcode/other/55-跳跃游戏.go
@@ -0,0 +1,16 @@
+package other
+
+
+func canJump(nums []int) bool {
+	lastMost  := 0
+	for i := 0; i < len(nums); i++ {
+		if i > lastMost {
+			return false
+		}
+		if i + nums[i] > lastMost {
+			lastMost = i + nums[i]
+		}
+	}
+
+	return true
+}
--- leetcode/other/test_test.go
+++ leetcode/other/test_test.go
@@ -9,3 +9,13 @@ func TestMaxProfit(t *testing.T)  {
 	v :=  maxProfit2(profits)
 	t.Log(v)
 }
+
+func TestCanJump(t *testing.T) {
+	nums := []int{3, 2, 1, 0, 4}
+	v := canJump(nums)
+	t.Log(v)
+
+	nums = []int{2,3,1,1,4}
+	v  = canJump(nums)
+	t.Log(v)
+}
