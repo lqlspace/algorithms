77c3f6fe341ace16c152f6de85874c4315e9006d
liuql <liuql_cs@163.com>
2021-10-20 19:48:42 +0800

    合并排序的数组

---
+++ leetcode/bytedance/10.01-合并排序的数组.go
@@ -0,0 +1,22 @@
+package bytedance
+
+
+func merge(A []int, m int, B []int, n int)  {
+	i, j, k := m-1, n-1, 1
+	for i >= 0 && j >= 0 {
+		if A[i] > B[j] {
+			A[m+n-k] = A[i]
+			i--
+		} else {
+			A[m+n-k] = B[j]
+			j--
+		}
+		k++
+	}
+
+	if j >= 0 {
+		for i := 0; i <= j; i++ {
+			A[i] = B[i]
+		}
+	}
+}
--- leetcode/bytedance/test_test.go
+++ leetcode/bytedance/test_test.go
@@ -28,3 +28,21 @@ func TestSendRedPackage(t *testing.T) {
 		fmt.Printf("%.2f ", float64(arr[i])/100)
 	}
 }
+
+func TestMerge(t *testing.T) {
+	a := []int{1,2,3,0,0,0}
+	b := []int{2,5,6}
+	merge(a, 3, b, 3)
+
+	for _, v := range a {
+		fmt.Printf("%d ", v)
+	}
+
+	a = []int{0}
+	b = []int{1}
+	merge(a, 0, b, 1)
+
+	for _, v := range a {
+		fmt.Printf("%d ", v)
+	}
+}
