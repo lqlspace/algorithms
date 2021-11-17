e5f7ec248f034c5d75fdbb6d404441816fcc968c
liuql <liuql_cs@163.com>
2021-11-17 23:05:21 +0800

    八皇后问题

---
+++ backtracking/backtracking_test.go
@@ -0,0 +1,9 @@
+package backtracking
+
+import (
+	"testing"
+)
+
+func TestCal8Queue(t *testing.T) {
+	Cal8Queue(0)
+}
---
+++ backtracking/cal8queue.go
@@ -0,0 +1,61 @@
+package backtracking
+
+var res = make([]int, 8)
+var cnt = 0
+
+func Cal8Queue(row int)  {
+	if row == 8 {
+		printQueues(res)
+		return
+	}
+
+	for col := 0; col < 8; col++ {
+		if isOk(row, col) {
+			res[row] =  col
+			Cal8Queue(row+1)
+		}
+	}
+}
+
+func isOk(row,  col int) bool {
+	leftup := col - 1
+	rightup := col + 1
+
+	for i := row-1; i >= 0; i-- {
+		if res[i] == col {
+			return  false
+		}
+		if leftup >= 0 {
+			if res[i]  == leftup {
+				return false
+			}
+		}
+
+		if rightup < 8 {
+			if res[i] == rightup {
+				return false
+			}
+		}
+
+		leftup--
+		rightup++
+	}
+	return  true
+}
+
+
+func printQueues(result []int)  {
+	for row :=  0; row < 8; row++ {
+		for col := 0; col < 8; col++ {
+			if result[row] == col {
+				print("Q  ")
+			} else {
+				print("* ")
+			}
+		}
+		println()
+	}
+	cnt++
+	println(cnt)
+}
+
