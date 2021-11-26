b7984158fce87267e65c15bcdb7954e743b7a607
liuql <liuql_cs@163.com>
2021-11-26 08:16:38 +0800

    二维数组中的查找

--- array/二维数组中的查找.go
+++ array/二维数组中的查找.go
@@ -5,20 +5,21 @@ package array
  */
 
 func Find(matrix [][]int, rows, columns, number int) bool {
-	var found bool
+	if matrix == nil || rows <= 0 || columns <= 0 {
+		return false
+	}
 
-	if matrix != nil && rows > 0 && columns > 0 {
-		row := 0
-		column := columns -1
-		for row < rows && column >= 0 {
-			if matrix[row][column]== number {
-				found = true
-				break
-			} else if matrix[row][column] > number {
-				column--
-			} else {
-				row++
-			}
+	var found bool
+	row := 0
+	column := columns - 1
+	for row < rows && column >= 0 {
+		if matrix[row][column] == number {
+			found = true
+			break
+		} else if matrix[row][column] > number {
+			column--
+		} else {
+			row++
 		}
 	}
 
