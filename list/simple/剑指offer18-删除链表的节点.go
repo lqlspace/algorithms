22623d6d759637c6d56a795ef7450fc69b53896c
liuql <liuql_cs@163.com>
2021-12-29 10:51:58 +0800

    删除节点操作

--- list/simple/剑指offer18-删除链表的节点.go
+++ list/simple/剑指offer18-删除链表的节点.go
@@ -1,25 +1,35 @@
 package simple
 
-/**
- * Definition for singly-linked list.
- * type ListNode struct {
- *     Val int
- *     Next *ListNode
- * }
- */
-func deleteNodeMethod1(head *ListNode, val int) *ListNode {
-	for head == nil {
-		return nil
-	}
-
+// 删除第一个节点
+func deleteFirstNode(head *ListNode, val int) *ListNode {
 	sentinel := new(ListNode)
 	sentinel.Next = head
 	pre := sentinel
+
 	for head != nil {
 		if head.Val == val {
 			pre.Next = head.Next
 			head.Next = nil
 			break
+		}
+		pre = head
+		head = head.Next
+	}
+
+	return sentinel.Next
+}
+
+// 删除所有值为val的节点
+func deleteAllNode(head *ListNode, val int) *ListNode {
+	sentinel := new(ListNode)
+	sentinel.Next = head
+	pre := sentinel
+
+	for head != nil {
+		if head.Val == val {
+			pre.Next = head.Next
+			head = pre.Next
+			continue
 		}
 		pre = head
 		head = head.Next
