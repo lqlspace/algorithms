6b59c5650c286afa697d1d2f22aac5c1dbf517ad
liuql <liuql_cs@163.com>
2022-02-15 14:14:04 +0800

    二叉树的中 序遍历

--- tree/二叉树的中序遍历.go
+++ tree/二叉树的中序遍历.go
@@ -1,16 +1,18 @@
 package tree
 
-func inorderTraversal(root *TreeNode) []int {
-	if root == nil {
-		return nil
+func inorderTraversal(root *TreeNode) (arr []int) {
+	var inorder func(root *TreeNode)
+	inorder = func(root *TreeNode) {
+		if root == nil {
+			return
+		}
+
+		inorder(root.Left)
+		arr =append(arr, root.Val)
+		inorder(root.Right)
 	}
 
-	var arr []int
-	left := inorderTraversal(root.Left)
-	arr = append(arr, left...)
-	arr = append(arr, root.Val)
-	right := inorderTraversal(root.Right)
-	arr = append(arr, right...)
+	inorder(root)
 
-	return arr
+	return
 }
