9906a36e011909d62cf21038f5e9d5ff0b6416f9
liuql <liuql_cs@163.com>
2022-02-18 11:35:18 +0800

    爬楼梯

--- dp/dp_test.go
+++ dp/dp_test.go
@@ -8,3 +8,8 @@ func Test_maxSubArray(t *testing.T)  {
 	arr := []int{2,1,-3,4,-1,2,1,-5,4}
 	t.Log(maxSubArray(arr))
 }
+
+func Test_climbStirs(t *testing.T) {
+	t.Log(climbStairs(2))
+	t.Log(climbStairs(3))
+}
