72fdb7fcfa2f002d855a07d73a3b76480a86ec33
liuql <liuql_cs@163.com>
2022-02-18 12:06:09 +0800

    买买股票的最佳时机

---
+++ dp/买卖股票的最佳时机.go
@@ -0,0 +1,20 @@
+package dp
+
+import (
+	"math"
+)
+
+// 方法二： 遍历一遍
+func maxProfit3(prices []int) int {
+	minPrices := math.MaxInt64
+	maxProfit := 0
+	for i := 0; i < len(prices); i++ {
+		if prices[i] < minPrices {
+			minPrices = prices[i]
+		} else if prices[i] - minPrices > maxProfit {
+			maxProfit = prices[i]
+		}
+	}
+
+	return maxProfit
+}
