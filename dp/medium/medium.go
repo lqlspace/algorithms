bd5b5be1e30297b245e1460a4416ad92b3913cf7
liuql <liuql_cs@163.com>
2022-02-26 13:13:43 +0800

    最长回文子串

--- dp/medium/medium.go
+++ dp/medium/medium.go
@@ -20,10 +20,55 @@ func longestPalindrome(s string) string {
 	return s[start:end+1]
 }
 
-func expandAroundCenter(s string, left, right int) (int, int) {
-	for left >= 0 && right < len(s) && s[left] == s[right] {
-		left, right = left-1, right+1
+func expandAroundCenter(s string, l, r int) (int, int) {
+	for l >= 0 && r <=len(s)-1 && s[l] == s[r] {
+		l, r = l-1, r+1
 	}
 
-	return left + 1, right - 1
+	return l+1, r-1
 }
+
+
+
+// 动态规划
+func longestPalindrome2(s string) string {
+	l := len(s)
+	if l < 2 {
+		return s
+	}
+
+	r := make([][]bool, l)
+	for i := 0; i < l; i++ {
+		r[i] = make([]bool, l)
+	}
+
+	ml, si := 1, 0
+	// 字串长度 sl = j - i + 1
+	for sl := 2; sl <= l; sl++ {
+		// 左边界 i
+		for i := 0; i < l; i++ {
+			// 右边界 j - i + 1 = sl
+			j := i + sl -1
+			if j >= l{
+				break
+			}
+			if s[i] != s[j] {
+				r[i][j] = false
+			} else {
+				if sl <= 3 {
+					r[i][j] = true
+				} else {
+					r[i][j] = r[i+1][j-1]
+				}
+			}
+
+			if r[i][j] && sl > ml {
+				ml = sl
+				si = i
+			}
+		}
+	}
+	return s[si:si+ml]
+}
+
+
--- dp/medium/medium_test.go
+++ dp/medium/medium_test.go
@@ -6,8 +6,13 @@ import (
 
 func Test_longestPalindrome(t *testing.T)  {
 	t.Log(longestPalindrome("ababcd"))
+	t.Log(longestPalindrome2("ababcd"))
 	t.Log(longestPalindrome("cbbd"))
+	t.Log(longestPalindrome2("cbbd"))
 	t.Log(longestPalindrome("a"))
+	t.Log(longestPalindrome2("a"))
 	t.Log(longestPalindrome(""))
+	t.Log(longestPalindrome2(""))
 	t.Log(longestPalindrome("abacdeeffe"))
+	t.Log(longestPalindrome2("abacdeeffe"))
 }
