fb888972a5623d00727dd085222990457319b2e2
liuql <liuql_cs@163.com>
2022-03-02 12:12:57 +0800

    螺旋矩阵

--- array/medium/螺旋矩阵.go
+++ array/medium/螺旋矩阵.go
@@ -2,34 +2,34 @@ package medium
 
 func spiralOrder(matrix [][]int) []int {
 	if len(matrix) == 0 || len(matrix[0]) == 0 {
-		return []int{}
+		return nil
 	}
 
 	rows, columns := len(matrix), len(matrix[0])
-	orders, idx := make([]int, rows * columns), 0
+	orders := make([]int, rows * columns)
 
 	left, right, top, bottom := 0, columns-1, 0, rows-1
-	for left <= right && top <= bottom {
+	for i := 0; left <= right && top <= bottom; {
 		for col := left; col <= right; col++ {
-			orders[idx] = matrix[top][col]
-			idx++
+			orders[i] = matrix[top][col]
+			i++
 		}
 		for row := top+1; row <= bottom; row++ {
-			orders[idx] = matrix[row][right]
-			idx++
+			orders[i] = matrix[row][right]
+			i++
 		}
 
 		if top < bottom {
 			for col := right-1; col >= left; col-- {
-				orders[idx] = matrix[bottom][col]
-				idx++
+				orders[i] = matrix[bottom][col]
+				i++
 			}
 		}
 
 		if left < right {
 			for row := bottom-1; row > top; row-- {
-				orders[idx] = matrix[row][left]
-				idx++
+				orders[i] = matrix[row][left]
+				i++
 			}
 		}
 
