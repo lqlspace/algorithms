f87ac9f9183d92eec9ea07e95199919dc8bd33d9
liuql <liuql_cs@163.com>
2022-03-06 09:14:33 +0800

    x的平方根

--- search/simple/simple_test.go
+++ search/simple/simple_test.go
@@ -10,3 +10,11 @@ func Test_search(t *testing.T)  {
 	t.Log(search([]int{-1,0,3,5,9,12}, 2))
 	t.Log(search2([]int{-1,0,3,5,9,12}, 2))
 }
+
+func Test_mySqrt(t *testing.T)  {
+	t.Log(mySqrt(4))
+	t.Log(mySqrt(8))
+	t.Log(mySqrt(9))
+	t.Log(mySqrt(15))
+	t.Log(mySqrt(16))
+}
---
+++ search/simple/x 的平方根.go
@@ -0,0 +1,24 @@
+package simple
+
+
+// 二分查找，递归
+func mySqrt(x int) int {
+	return binarySqrt(x, 0, x)
+}
+
+func binarySqrt(x, low, high int) int {
+	if low > high {
+		return high
+	}
+
+	mid := low + (high-low) >> 1
+	if mid * mid == x {
+		return mid
+	} else if mid * mid > x {
+		high = mid-1
+	} else {
+		low = mid + 1
+	}
+
+	return binarySqrt(x, low, high)
+}
