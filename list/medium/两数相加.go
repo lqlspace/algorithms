4afb8906667a24174237f85a46014403d97e5ed7
liuql <liuql_cs@163.com>
2022-03-09 19:00:58 +0800

    两数相加

--- list/medium/两数相加.go
+++ list/medium/两数相加.go
@@ -6,6 +6,12 @@ package medium
  */
 
 func addTwoNumbers2(l1, l2 *ListNode) *ListNode {
+	if l1 == nil {
+		return l2
+	} else if l2 == nil {
+		return l1
+	}
+
 	dummy := new(ListNode)
 	pre := dummy
 
@@ -27,39 +33,4 @@ func addTwoNumbers2(l1, l2 *ListNode) *ListNode {
 	}
 
 	return dummy.Next
-}
-
-
-// 时间复杂度O(MAX(M, N))， 空间复杂度O（MAX(M, N)）
-func addTwoNumbers(l1 *ListNode, l2 *ListNode) *ListNode {
-	sentinel := new(ListNode)
-	last := sentinel
-
-	var carry int
-	for l1 != nil || l2 != nil {
-		sum := carry
-		if l1 != nil {
-			sum += l1.Val
-			l1 = l1.Next
-		}
-		if l2 != nil {
-			sum += l2.Val
-			l2 = l2.Next
-		}
-
-		carry = sum / 10
-		node := new(ListNode)
-		node.Val = sum % 10
-		last.Next = node
-		last = last.Next
-	}
-
-	if carry > 0 {
-		node := new(ListNode)
-		node.Val = carry
-		last.Next = node
-		last = last.Next
-	}
-
-	return sentinel.Next
 }
