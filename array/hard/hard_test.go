1dbe1a46cb5a3149bcd68627109328ef37d7995f
liuql <liuql_cs@163.com>
2022-03-14 15:04:11 +0800

    滑动窗口最大值

--- array/hard/hard_test.go
+++ array/hard/hard_test.go
@@ -6,5 +6,8 @@ import (
 
 func Test_maxSlidingWindow(t *testing.T)  {
 	t.Log(maxSlidingWindow([]int{1,3,-1,-3,5,3,6,7}, 3))
+	t.Log(maxSlidingWindow2([]int{1,3,-1,-3,5,3,6,7}, 3))
 	t.Log(maxSlidingWindow([]int{1}, 1))
+	t.Log(maxSlidingWindow2([]int{1}, 1))
+	t.Log(maxSlidingWindow([]int{5, 3, 4}, 1))
 }
--- array/hard/滑动窗口最大值.go
+++ array/hard/滑动窗口最大值.go
@@ -1,6 +1,65 @@
 package hard
 
-// 1. 暴力法
+import (
+	"container/heap"
+)
+
+// 1. 优先队列
+type Item struct {
+	Val int
+	Index int
+}
+type PriorityQueue struct {
+	Items []*Item
+}
+
+func (pq *PriorityQueue) Len() int {
+	return len(pq.Items)
+}
+
+func (pq *PriorityQueue) Less(i, j int) bool {
+	return pq.Items[i].Val > pq.Items[j].Val
+}
+
+func (pq *PriorityQueue) Swap(i, j int) {
+	pq.Items[i], pq.Items[j] = pq.Items[j], pq.Items[i]
+}
+
+func (pq *PriorityQueue) Push(x interface{}) {
+	item := x.(*Item)
+	pq.Items = append(pq.Items, item)
+}
+
+func (pq *PriorityQueue) Pop() interface{} {
+	item := pq.Items[len(pq.Items)-1]
+	pq.Items = pq.Items[:len(pq.Items)-1]
+
+	return item
+}
+
+
+func maxSlidingWindow(nums []int, k int) []int {
+	pq := new(PriorityQueue)
+	for i := 0; i < k; i++ {
+		pq.Push(&Item{Val:nums[i], Index:i})
+	}
+
+	heap.Init(pq)
+	ans := []int{pq.Items[0].Val}
+	for i := k; i < len(nums); i++ {
+		heap.Push(pq, &Item{Val:nums[i], Index:i})
+		for pq.Items[0].Index < i - k + 1 {
+			heap.Pop(pq)
+		}
+
+		ans = append(ans, pq.Items[0].Val)
+	}
+
+	return ans
+}
+
+
+// 2. 暴力法
 func maxSlidingWindow2(nums []int, k int) []int {
 	n := len(nums)
 
