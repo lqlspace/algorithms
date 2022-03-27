ca50a91673d852781a476ae66f1d1fb85b3ae42d
liuql <liuql_cs@163.com>
2022-03-27 22:57:08 +0800

    子集

---
+++ array/medium/78-子集.go
@@ -0,0 +1,16 @@
+package medium
+
+
+func subsets(nums []int) (ans [][]int) {
+	n := len(nums)
+	for mask := 0; mask < 1<<n; mask++ {
+		set := []int{}
+		for i, v := range nums {
+			if mask>>i&1 > 0 {
+				set = append(set, v)
+			}
+		}
+		ans = append(ans, append([]int(nil), set...))
+	}
+	return
+}
