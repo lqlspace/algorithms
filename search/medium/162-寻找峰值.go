109d61687197984613b817cab0dec007fe3a2a52
liuql <liuql_cs@163.com>
2022-04-06 22:51:16 +0800

    寻找峰值

---
+++ search/medium/162-寻找峰值.go
@@ -0,0 +1,13 @@
+package medium
+
+func findPeakElement(nums []int) int {
+	n := len(nums)
+
+	for i := 0; i < n-1; i++ {
+		if nums[i] > nums[i+1] {
+			return i 
+		}
+	}
+
+	return n - 1
+}
--- search/medium/medium_test.go
+++ search/medium/medium_test.go
@@ -2,6 +2,8 @@ package medium
 
 import (
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func Test_search(t *testing.T)  {
@@ -17,4 +19,10 @@ func Test_searchRange(t *testing.T) {
 	t.Log(searchRange([]int{5,7,7,8,8,10}, 8))
 	t.Log(searchRange([]int{5,7,7,8,8,10}, 6))
 	t.Log(searchRange([]int{}, 0))
+}
+
+
+func Test_findPeakElement(t *testing.T) {
+	idx := findPeakElement([]int{1, 3, 2, 4, 5})
+	assert.Equal(t, 1, idx)
 }
